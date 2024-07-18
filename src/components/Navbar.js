be6b0aaf61b3156968db5773445eaba0b58d16e0
רועי נזרי <roeinizri1@gmail.com>
2024-07-18 19:15:58 +0300

    Your commit message

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => (
       <li><Link to="/">Home</Link></li>
       <li><Link to="/skills">Skills</Link></li>
       <li><Link to="/General">General</Link></li>
-      <li><Link to="/projects">Projects</Link></li>
+      <li><Link to="/projects">Portfolio</Link></li>
       <li><Link to="/contact">Contact</Link></li>
     </ul>
   </nav>
