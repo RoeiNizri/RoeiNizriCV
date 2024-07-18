4baff159e9217dca7317d48128a359f4e425fed6
רועי נזרי <roeinizri1@gmail.com>
2024-07-18 14:40:01 +0300

    Initial commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import styles from './Contact.module.css';
-import profilePicture from '../ran_photo.jpeg'; // Adjusted path to src folder
+import profilePicture from '../RoeiNizriImg.jpg'; // Adjusted path to src folder
 
 const Contact = () => (
   <section className={styles.contact}>
     <div className={styles.container}>
       <h2>Contact</h2>
       <img src={profilePicture} alt="Profile" className={styles['profile-picture']} />
-      <p>Email: <a href="mailto:ranpolac@gmail.com">ranpolac@gmail.com</a></p>
-      <p>Phone: <a href="tel:054-5217456">054-5217456</a></p>
-      <p>LinkedIn: <a href="https://www.linkedin.com/in/ran-polac" target="_blank" rel="noopener noreferrer">https://www.linkedin.com/in/ran-polac</a></p>
-      <p>GitHub: <a href="https://github.com/Ranash12P" target="_blank" rel="noopener noreferrer">https://github.com/Ranash12P</a></p>
+      <div className={styles.contactInfo}>
+        <p><strong>Email:</strong> <a href="mailto:roeinizri1@gmail.com">roeinizri1@gmail.com</a></p>
+        <p><strong>Phone:</strong> <a href="tel:054-694-5333">054-694-5333</a></p>
+        <p><strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/RoeiNizri" target="_blank" rel="noopener noreferrer">Roei Nizri</a></p>
+        <p><strong>GitHub:</strong> <a href="https://github.com/RoeiNizri" target="_blank" rel="noopener noreferrer">RoeiNizri</a></p>
+      </div>
     </div>
   </section>
 );
