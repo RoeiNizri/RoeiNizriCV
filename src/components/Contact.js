cb08d742cac8caa5888df03464861988f3f2c1a5
רועי נזרי <roeinizri1@gmail.com>
2024-07-20 14:25:01 +0300

    Update 20/07/2024

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,15 +5,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import downloadIcon from '../logo512.png'; // Import the new image
 
-const handleDownload = () => {
-  const link = document.createElement('a');
-  link.href = '/RoeiNizri_Resume.pdf';
-  link.setAttribute('download', 'RoeiNizri_Resume.pdf');
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-};
-
 const Contact = () => (
   <section className={styles.contact}>
     <div className={styles.container}>
@@ -23,19 +14,19 @@ const Contact = () => (
         <p><strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/RoeiNizri" target="_blank" rel="noopener noreferrer">Roei Nizri</a></p>
         <p><strong>Email:</strong> <a href="mailto:roeinizri1@gmail.com">roeinizri1@gmail.com</a></p>
         <p>
-          <strong>WhatsApp:</strong> <a href="https://wa.me/972546945333" target="_blank" rel="noopener noreferrer">
+          <strong>WhatsApp:</strong> <a href="https://wa.me/0546945333" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faWhatsapp} size="lg" style={{ marginRight: '8px', color: '#25D366' }} />
             Send me a message
           </a>
         </p>
-        <p><strong>Phone:</strong> <a href="tel:+972-54-694-5333">054-694-5333</a></p>
+        <p><strong>Phone:</strong> <a href="tel:054-694-5333">054-694-5333</a></p>
       </div>
       <div className={styles.resumeSection}>
         <h3>Download My Resume</h3>
         <p>
-          <button onClick={handleDownload} className={styles.downloadButton}>
+          <a href="../RoeiNizri_Resume.pdf" download="RoeiNizri_Resume.pdf">
             <img src={downloadIcon} alt="Download Resume" className={styles.downloadIcon} />
-          </button>
+          </a>
         </p>
       </div>
     </div>
