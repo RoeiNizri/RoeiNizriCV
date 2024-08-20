1b1ed66d76a8acadafd62d7176def40fbeb3cf4e
רועי נזרי <roeinizri1@gmail.com>
2024-08-20 17:16:30 +0300

    Updates

--- 404.js
+++ 404.js
@@ -1,14 +1,3 @@
-// Load the main JavaScript file
-const script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = '/static/js/main.4939a931.js';
-
-// Once the script is loaded, proceed with the redirect
-script.onload = function() {
-    setTimeout(function() {
-        window.location.href = window.location.origin;
-    }, 10000); // 10 seconds delay
-};
-
-// Append the script to the head
-document.head.appendChild(script);
+setTimeout(function() {
+    window.location.href = window.location.origin;
+}, 10000); // 10 seconds delay
