1d66c23d528ffb86bef6740ea467a88857150170
רועי נזרי <roeinizri1@gmail.com>
2024-08-20 16:58:38 +0300

    Updates

--- scripts.js
+++ scripts.js
@@ -6,4 +6,14 @@ const timeout = setTimeout(function() {
   window.onload = function() {
     clearTimeout(timeout);
   };
-  
+
+// Load the main JavaScript file
+const script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = '/static/js/main.4939a931.js';
+document.head.appendChild(script);
+
+// Redirect to the main site after a short delay
+setTimeout(function() {
+    window.location.href = window.location.origin;
+}, 10000); // 10 seconds delay
